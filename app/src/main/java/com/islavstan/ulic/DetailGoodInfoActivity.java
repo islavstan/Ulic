36136ebf5af68f369405abf6f1d2e947c09f41fa
islavstan <islavstan@gmail.com>
2017-02-07 15:48:33 +0300

    07.02.17

--- app/src/main/java/com/islavstan/ulic/DetailGoodInfoActivity.java
+++ app/src/main/java/com/islavstan/ulic/DetailGoodInfoActivity.java
@@ -1,8 +1,10 @@
 package com.islavstan.ulic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 import com.islavstan.ulic.adapter.LoopAdapter;
@@ -14,6 +16,7 @@ public class DetailGoodInfoActivity extends AppCompatActivity {
     TextView condition;
     TextView price;
     TextView goodsInfo;
+    TextView buyBtn;
 
 
     @Override
@@ -31,6 +34,12 @@ public class DetailGoodInfoActivity extends AppCompatActivity {
         rollPagerView.setAdapter(adapter);
         // rollPagerView.pause();
 
-
+          buyBtn = (TextView) findViewById(R.id.buyBtn);
+        buyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DetailGoodInfoActivity.this,BuyGoodsActivity.class));
+            }
+        });
     }
 }
